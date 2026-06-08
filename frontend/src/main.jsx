f28872922252f2317c8cbda1a6785b4046a6657e
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-09 00:44:00 +0530

    feat: wrap App component with AppointmentProvider and React.StrictMode

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,13 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
+import { AppointmentProvider } from './context/AppointmentContext'
 
-createRoot(document.getElementById('root')).render(
-    <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppointmentProvider>
+      <App />
+    </AppointmentProvider>
+  </React.StrictMode>,
 )
