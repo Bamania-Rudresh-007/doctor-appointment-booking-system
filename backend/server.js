77022c8de540e365c01b1f3e538bd1cbc2b152f4
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-11 14:39:22 +0530

    feat: set up Express server with CORS and JSON middleware

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import "dotenv/config"
+import connectDB from "./src/config/db.js";
+import { corsOptions } from "./src/config/corsConfig.js";
+
+const app = express();
+const port = process.env.PORT || 7000;
+
+app.use(cors(corsOptions));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.end("Hey welcome to the carefirst clinic backend api's !!!...");
+})
+
+// connectDB();
+
+app.listen(port, () =>{
+    console.log(`Server is listening on http://localhost:${port}`);
+})
