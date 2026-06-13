a573045a1f09bdb1ce2816b0875121e8baf01e0f
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-13 14:58:54 +0530

    feat: add route to retrieve list of admins in authentication module

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { login, refreshAccessToken, logout, registerAdmin } from "../controllers/auth.controller.js";
+import { login, refreshAccessToken, logout, registerAdmin, getAdmins } from "../controllers/auth.controller.js";
 
 const authRouter = express.Router();
 
 authRouter.post("/register", registerAdmin);
+authRouter.get("/get-admins", getAdmins);
 authRouter.post("/login", login);
 authRouter.post("/refresh", refreshAccessToken);
 authRouter.post("/logout", logout);
