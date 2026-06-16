79be7968db833b4827126b3257889807df5170e3
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-16 19:34:44 +0530

    feat: implement updateAppointment function for modifying existing appointments

--- backend/src/controllers/appointment.controller.js
+++ backend/src/controllers/appointment.controller.js
@@ -34,5 +34,47 @@ const registerAppointment = async (req, res) => {
     }
 };
 
+const updateAppointment = async (req, res) => {
+    try {
+        console.log("Request Body:" ,req.body)
+        const { _id, changes } = req.body;
 
-export { registerAppointment };
+        if (!_id) {
+            return res.status(400).json({
+                success: false,
+                message: "Id is required"
+            });
+        }
+
+        const updatedAppointment = await Appointment.findByIdAndUpdate(
+            _id, 
+            changes,
+            { returnDocument: "after", runValidators: true } 
+        );
+
+        if (!updatedAppointment) {
+            return res.status(404).json({
+                success: false,
+                message: "Appointment not found"
+            });
+        }
+
+        return res.status(200).json({
+            success: true,
+            message: "Appointment updated successfully",
+            data: {
+                appointment: updatedAppointment 
+            }
+        });
+
+    } catch (err) {
+        console.error("Error Updating appointment: ", err);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+        });
+    }
+};
+
+
+export { registerAppointment, updateAppointment };
