6e7f2b29999905a84c68764731c43d8019342ae0
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-16 19:38:35 +0530

    feat: add authentication middleware to appointment routes

--- backend/src/routes/appointment.route.js
+++ backend/src/routes/appointment.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { registerAppointment, updateAppointment, getAllAppointments } from "../controllers/appointment.controller.js";
+import protect from "./src/middlewares/protect.middleware.js";
 
 const appointmentRouter = express.Router();
 
-appointmentRouter.post("/register", registerAppointment);
-appointmentRouter.put("/update", updateAppointment);
-appointmentRouter.get("/all", getAllAppointments);
+appointmentRouter.post("/register", protect, registerAppointment);
+appointmentRouter.put("/update", protect, updateAppointment);
+appointmentRouter.get("/all", protect, getAllAppointments);
 
 export default appointmentRouter;
