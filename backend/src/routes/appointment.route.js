81aa878c145def8ece96301647dab171adbdb1e7
Rudresh <bamaniarudresh2008@gmail.com>
2026-06-17 19:46:28 +0530

    fix: remove protect middleware from register appointment route

--- backend/src/routes/appointment.route.js
+++ backend/src/routes/appointment.route.js
@@ -1,11 +1,12 @@
 import express from "express";
-import { registerAppointment, updateAppointment, getAllAppointments } from "../controllers/appointment.controller.js";
+import { registerAppointment, updateAppointment, getAllAppointments, getLatestAppointmentTime } from "../controllers/appointment.controller.js";
 import protect from "../middlewares/protect.middleware.js"
 
 const appointmentRouter = express.Router();
 
-appointmentRouter.post("/register", protect, registerAppointment);
+appointmentRouter.post("/register", registerAppointment);
 appointmentRouter.put("/update", protect, updateAppointment);
 appointmentRouter.get("/all", protect, getAllAppointments);
+appointmentRouter.get("/latest", protect, getLatestAppointmentTime);
 
 export default appointmentRouter;
